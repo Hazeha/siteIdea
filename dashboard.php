40b139a2df201725f72eaaf27751838a905d5863
NikoG <Krantja@gmail.com>
2017-05-25 18:16:55 +0200

    Undone commit. Dashboard Paneller / Elementer rykkes til en fil. (dashboard.php)

--- dashboard.php
+++ dashboard.php
@@ -71,23 +71,32 @@
                 </li>
             </ul>
         </nav><!--Header Slut-->
-        <div id="sidebar-wrapper">
+        
+
+
+
+
+
+
+
+
+<div id="sidebar-wrapper">
             <!--Side Nav Start-->
             <ul class="sidebar-nav">
                 <li class="sidebar-sitepage">
-                    <h2>Profile</h2>
+                    <h2><strong>Help</strong></h2>
                 </li>
                 <li>
-                    <a href="dashboard.php">Dashboard</a>
+                    <a data-toggle="tab" href="#dashboard">Dashboard</a>
                 </li>
                 <li>
-                    <a href="scriptport.php">View Scripts</a>
+                    <a data-toggle="tab" href="#scripts">View Scripts</a>
                 </li>
                 <li>
-                    <a href="#">Upload Script</a>
+                    <a data-toggle="tab" href="#upload">Upload Script</a>
                 </li>
                 <li>
-                    <a href="#">Settings</a>
+                    <a data-toggle="tab" href="#settings">Settings</a>
                 </li>
                 <li>
                     <a href="#">Logout</a>
@@ -95,60 +104,176 @@
                 <li class="divider"></li>
             </ul>
         </div><!--Side Nav Slut-->
+
+
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header"><strong>Dashboard</strong></h1>
+                    
                 </div>
             </div>
         </div>
         <div class="container-fluid">
             <!-- Games Here-->
             <div class="row">
+                
+                 <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="tab-content">
+                                    <div class="tab-pane fade in active" id="dashboard">
+                                        <center><h2>Xhazes - Legend Content Creator</h2></center>
+                                        <hr>
+                                        <!-- Post Content -->
+
+            
+<div class="col-lg-8">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="col-sm-3">
+                <div class="panel panel-default">
+                    <img class="img-thumbnail" src="images/pic01.png"> <button class="btn btn-default">Change Picture</button>
+                </div>
+                <ul>
+                    <li>Nickname : Nickname</li>
+                    <li>Join Date : Date</li>
+                    <li>Reputation : rep</li>
+                    <li>Scripts : 12</li>
+                    <li>Rating : 10/10</li>
+                </ul>
+            </div>
+            <div class="col-lg-9">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Biography
+                    </div>
+                    <div class="panel-heading pull-right">
+                        Scripts
+                    </div>
+                    <div class="panel-body">
+                        <div class="col-lg-4">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <p>Lidt om ham som scripter, til dem som køber, eller vil hyre vedkommende til at lave deres job</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 pull-right">
+                            <p>Kolloner med scripts til salg, eller købt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.panel-body -->
+    </div>
+</div>
+<div class="col-lg-4 pull-right">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <i class="fa fa-bell fa-fw"></i> Stat Panel
+        </div><!-- /.panel-heading -->
+        <div class="panel-body">
+            <div class="list-group">
+                <a class="list-group-item" href="#"><i class="fa fa-comment fa-fw"></i> Scripts for sale <span class="pull-right text-muted small"><em>4</em></span></a> <a class="list-group-item" href="#"><i class="fa fa-twitter fa-fw"></i> Updated scripts <span class="pull-right text-muted small"><em>9001</em></span></a> <a class="list-group-item" href="#"><i class="fa fa-shopping-cart fa-fw"></i> Brought Scripts <span class="pull-right text-muted small"><em>9</em></span></a> <a class="list-group-item" href="#"><i class="fa fa-shopping-cart fa-fw"></i> Sold Scripts <span class="pull-right text-muted small"><em>9</em></span></a> <a class="list-group-item" href="#"><i class="fa fa-money fa-fw"></i> Payments <span class="pull-right text-muted small"><em>1337</em></span></a>
+            </div>
+        </div><!-- /.panel-body -->
+    </div>
+    <div class="chat-panel panel panel-default">
+        <div class="panel-heading">
+            <i class="fa fa-comments fa-fw"></i> Support
+            <div class="btn-group pull-right">
+                <button class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" type="button"><i class="fa fa-chevron-down"></i></button>
+                <ul class="dropdown-menu slidedown">
+                    <li>
+                        <a href="#"><i class="fa fa-refresh fa-fw"></i> Answer Tickets</a>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- /.panel-heading -->
+        <div class="panel-body">
+            <ul class="chat">
+                <li class="left clearfix">
+                    <div class="chat-body clearfix">
+                        <div class="header">
+                            <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted"><i class="fa fa-clock-o fa-fw"></i> 12 mins ago</small>
+                        </div>
+                        <p>Harro!</p>
+                    </div>
+                </li>
+            </ul>
+        </div><!-- /.panel-body -->
+        <div class="panel-footer">
+            <div class="input-group">
+                <input class="form-control input-sm" id="btn-input" placeholder="Type your message here..." type="text"> <span class="input-group-btn"><button class="btn btn-warning btn-sm" id="btn-chat"><span class="input-group-btn"><span class="input-group-btn">Send</span></span></button></span>
+            </div>
+        </div><!-- /.panel-footer -->
+    </div>
+</div>
+</div>
+                
+            
 
 
-
-                <div class="col-lg-8">
+                                        
+                                    <div class="tab-pane fade" id="scripts">
+                                        <center><h2>View Scripts</h2></center>                                        
+                                        <hr>
+                                        <div>
+                                        <div class="col-lg-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Profile - Profile Name
+                            Brought scripts
                         </div><!-- /.panel-heading -->
                         <div class="panel-body">
-                            <div class="col-sm-3">
-                                <div class="panel panel-default">
-                                    <img src="images/pic01.png" class="img-thumbnail"> 
-                                    <button class="btn btn-default">Change Picture</button>
+                            
+                                <div class="panel-heading pull-right"><strong>Script Name</strong><hr>
+                                    <li>
+                                        Price :
+                                    </li>
+                                    <li>
+                                        Sales :
+                                    </li>
+                                    <li>
+                                        Date Added :
+                                    </li>
+                                    <li>
+                                        Game :
+                                    </li>
+                                    <li>
+                                        <a href="#">Put on Sale</a>
+                                    </li>
                                 </div>
-                                <ul>
-                                    <li>Nickname : Nickname</li>
-                                    <li>Join Date : Date</li>
-                                    <li>Reputation : rep</li>
-                                    <li>Scripts : 12</li>
-                                    <li>Rating : 10/10</li>
-                                </ul>
-                            </div>
-                            <div class="col-lg-9">
-                                <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                        Biography
-                                    </div>
-                                        <div class="panel-heading pull-right">Scripts</div>
-                                        <div class="panel-body">
-                                            <div class="col-lg-4">
-                                                <div class="panel panel-default">
-                                                    <div class="panel-body">
-                                                        <p>Lidt om ham som scripter, til dem som køber, eller vil hyre vedkommende til at lave deres job</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-5 pull-right">
-
-                                                <P>Kolloner med scripts til salg, eller købt.</P>
-                                            </div>
-                                        </div>                        
-                                </div>
-                            </div>
+                                <img src="img/fallout-logo.jpg" class="img-thumbnail" height="150px" width="300px">
+                            
                         </div><!-- /.panel-body -->
+
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                            
+                            <div class="panel-heading pull-right"><strong>Script Name</strong><hr>
+                            <li>
+                                Price :
+                            </li>
+                            <li>
+                                Sales :
+                            </li>
+                            <li>
+                                Date Added :
+                            </li>
+                            <li>
+                                Game :
+                            </li>
+                            <li>
+                                <a href="#">Put on Sale</a>
+                            </li>
+                            </div>
+                                
+                                <img src="img/fallout-logo.jpg" class="img-thumbnail" height="150px" width="300px">
+                                
+                            
+                        </div><!-- /.panel-body -->
+                        </div>
                     </div>
                 </div>
 
@@ -160,53 +285,105 @@
 
 
 
-                <div class="col-lg-4 pull-right">
+                <div class="col-lg-6 pull-right">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <i class="fa fa-bell fa-fw"></i> Stat Panel
+                           Sold Scripts
                         </div><!-- /.panel-heading -->
                         <div class="panel-body">
-                            <div class="list-group">
-                                <a class="list-group-item" href="#"><i class="fa fa-comment fa-fw"></i> Scripts for sale <span class="pull-right text-muted small"><em>4</em></span>
-                                </a> <a class="list-group-item" href="#"><i class="fa fa-twitter fa-fw"></i> Updated scripts <span class="pull-right text-muted small"><em>9001</em></span></a> 
-                                <a class="list-group-item" href="#"><i class="fa fa-shopping-cart fa-fw"></i> Brought Scripts <span class="pull-right text-muted small"><em>9</em></span></a> 
-                                <a class="list-group-item" href="#"><i class="fa fa-shopping-cart fa-fw"></i> Sold Scripts <span class="pull-right text-muted small"><em>9</em></span></a> 
-                                <a class="list-group-item" href="#"><i class="fa fa-money fa-fw"></i> Payments <span class="pull-right text-muted small"><em>1337</em></span></a>
-                            </div>
-                        </div><!-- /.panel-body -->
-                    </div>
-                    <div class="chat-panel panel panel-default">
-                        <div class="panel-heading">
-                            <i class="fa fa-comments fa-fw"></i> Support
-                            <div class="btn-group pull-right">
-                                <button class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" type="button"><i class="fa fa-chevron-down"></i></button>
-                                <ul class="dropdown-menu slidedown">
+                            
+                                <div class="panel-heading pull-right"><strong>Script Name</strong><hr>
                                     <li>
-                                        <a href="#"><i class="fa fa-refresh fa-fw"></i> Answer Tickets</a>
+                                        Price : 9$
                                     </li>
-                                </ul>
-                            </div>
-                        </div><!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <ul class="chat">
-                                <li class="left clearfix">
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted"><i class="fa fa-clock-o fa-fw"></i> 12 mins ago</small>
-                                        </div>
-                                        <p>Harro!</p>
-                                    </div>
-                                </li>
-                            </ul>
+                                    <li>
+                                        Update Date : 10 days ago
+                                    </li>
+                                    <li>
+                                        Date Added : 01-01-2017
+                                    </li>
+                                    <li>
+                                        Game : Fallout
+                                    </li>
+                                    <li>
+                                        <a href="#">Update Avaible</a>
+                                    </li>
+                                </div>
+                                <img src="img/fallout-logo.jpg" class="img-thumbnail" height="150px" width="300px">
+                            
                         </div><!-- /.panel-body -->
-                        <div class="panel-footer">
-                            <div class="input-group">
-                                <input class="form-control input-sm" id="btn-input" placeholder="Type your message here..." type="text"> <span class="input-group-btn"><button class="btn btn-warning btn-sm" id="btn-chat"><span class="input-group-btn">Send</span></button></span>
-                            </div>
-                        </div><!-- /.panel-footer -->
+
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                            
+                            <div class="panel-heading pull-right"><strong>Script Name</strong><hr>
+                            <li>
+                                        Price : 9$
+                                    </li>
+                                    <li>
+                                        Update Date : 10 days ago
+                                    </li>
+                                    <li>
+                                        Date Added : 01-01-2017
+                                    </li>
+                                    <li>
+                                        Game : Fallout
+                                    </li>
+                                    <li>
+                                        <a href="#">Update Avaible</a>
+                                    </li>                            </div>
+                                
+                                <img src="img/fallout-logo.jpg" class="img-thumbnail" height="150px" width="300px">
+                            
+                    
+
+
+                                    </div>                                    
+                                    <div class="tab-pane fade" id="upload">
+                                            <center><h2>Upload Script</h2>                                      
+                                            <hr>
+                                            
+
+                                        <p></p></center>  
+                                    </div>
+                                    <div class="tab-pane fade" id="settings">
+                                        <center><h2>Settings</h2>
+                                            <hr>
+                                        <p></p></center>    
+                                    </div>                                    
+                                </div>
+                            </div><!-- /.panel-body -->
+                        </div><!-- Preview Image -->
+                        <!-- Blog Comments -->
+                        <!-- Comments Form -->
                     </div>
-                </div>
-            </div><!-- Footer -->
+
+
+            </div>
+        </div><
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+            <!-- Footer -->
             <footer id="footer">
                 <ul class="menu">
                     <li>
