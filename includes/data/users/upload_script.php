0d29190e42dea931eff6f1644faa5f8cdcb57f02
NikoG <Krantja@gmail.com>
2017-10-03 08:45:13 +0200

    no message

--- includes/data/users/upload_script.php
+++ includes/data/users/upload_script.php
@@ -1,127 +1,53 @@
 
+<div class="col-lg-6">
+	<div class="panel panel-default">
+		<div class="panel-body">
+        <form action="scriptupload.php" method="post">
+			<div class="form-group">
+  				<label>Script Name</label>
+  				<input name="name_input" type="text" class="form-control" placeholder="Example : Taxi Mod">
 
+  				<label>Short Description</label>
+  				<textarea name="discription_input" type="text" class="form-control" rows="2" placeholder="Max 250"></textarea>
 
-                                        <center>
-                                        	<div class="col-lg-6">
-												<div class="panel panel-default">
-													<div class="panel-body">
-														<div class="form-group">
-                                            				<label>Script Name</label>
-                                            				<input class="form-control" placeholder="Example : Taxi Mod">
-                                        				</div>
+  				<label>Features</label>
+  				<textarea name="features_input" type="text" class="form-control" rows="5" placeholder="Long description"></textarea>
 
-                                        				<div class="form-group">
-                                            				<label>Short Description</label>
-                                            				<textarea class="form-control" rows="2"></textarea>
-                                        				</div>
+  				<label>Price</label>
+  				<input name="price_input" type="double" class="form-control" placeholder="Example : 12.5">
 
-                                        				<div class="form-group">
-                                            				<label>Features</label>
-                                            				<textarea class="form-control" rows="5"></textarea>
-                                        				</div>
-
-                                        				<div class="form-group">
-                                            				<label>Price</label>
-                                            				<input class="form-control" placeholder="Example : 2">
-                                        				</div>
-
-                                        				<div class="form-group">
-                                            				<label>Game</label>
-                                            				<select class="form-control">
-                                                			<option>GTA</option>
-                                                			<option>Arma I</option>
-                                                			<option>Arma II</option>
-                                            				</select>
-                                        				</div>
-                                                        <label>Category</label><p> Try to select the categories that match your script the most</p>
-
-
-                                        				<div class="form-group col-lg-3">                                                                                                   					
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Weapons
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Maps
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Model
-                                                                </label>
-                                                            </div>
-                                                        </div>
-
-
-                                                        <div class="form-group col-lg-3">
-                                                                
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Roleplaying
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Admin
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">UI
-                                                                </label>
-                                                            </div>
-                                                        </div>
-
-
-                                                        <div class="form-group col-lg-3">
-                                                                
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Chat
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Donation
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Vehicle
-                                                                </label>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group col-lg-3">
-                                                                
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">inventory
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Scoreboard
-                                                                </label>
-                                                            </div>
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" value="">Gamemode
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                        <label>Mod Upload</label><p>We only accept zip compressed files</p>
-                                                        <input type="file">
-                                                        </div>
-
-
-                                                        <button type="submit" class="btn btn-default pull-left">Submit Button</button>
-
-
-                                                    </div>
-                                                </div>
-                                        	</div>
-                                        </center>
+  				<label>Game</label>
+  				<select name="game_input" class="form-control">
+      			<option>GTA</option>
+      			<option>Arma I</option>
+      			<option>Arma II</option>
+  				</select>
+	    </form>
+              <label>Category</label><p> Try to select the categories that match your script the most</p>
+		        <div class="form-group col-lg-6">                                                                                                   					
+                  <div class="checkbox">
+                      <label>
+                          <input type="checkbox" value="">Weapons
+                      </label>
+                  </div>
+                  <div class="checkbox">
+                      <label>
+                          <input type="checkbox" value="">Maps
+                      </label>
+                  </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="">Model
+                        </label>
+                    </div>
+                </div>
+               </div>
+                <hr>
+                
+                <label>Mod Upload</label><p>We only accept zip compressed files</p>
+                <input type="file">
+                
+                <button type="submit" class="btn btn-default pull-left">Submit Button</button>
+            </div>
+        </div>
+	</div>
